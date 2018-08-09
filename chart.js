8c7bf806d9b2bfdc9d028a678167a3d058aa1865
korinaal <korinaaaki97@gmail.com>
2018-08-09 18:19:06 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -68,13 +68,23 @@ function transition(name) {
 		$("#view-donor-type").fadeIn(1000);
 		return donorType();
 	}
-	if (name === "group-by-money-source")
+	if (name === "group-by-money-source") {
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeIn(1000);
 		return fundsType();
+	}
+	if (name === "group-by-the-amount-of-the-donation")
+		$("#initial-content").fadeOut(250);
+		$("#value-scale").fadeOut(250);
+		$("#view-donor-type").fadeOut(250);
+		$("#view-party-type").fadeOut(250);
+		$("#view-source-type").fadeIn(250);
+		$("#vnew-view").fadeIn(250);
+		return fundsType();
+	  
 	}
 
 function start() {
